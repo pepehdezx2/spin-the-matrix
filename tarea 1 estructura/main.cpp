fb7cdaf8e6661c4d477896b35bc38364e1fa3148
Pepe hdez <pepehdezhdez@pepes-macbook-pro.local>
2018-08-07 11:57:40 -0500

    Initial Commit

---
+++ tarea 1 estructura/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tarea 1 estructura
+//
+//  Created by Pepe Hdez hdez on 8/7/18.
+//  Copyright © 2018 Pepe Hdez hdez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
