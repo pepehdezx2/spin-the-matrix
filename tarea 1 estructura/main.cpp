f5da6d1916c6dda33923cfe5ef518694d5216c6a
José Antonio Hernández <pepehdezx2@gmail.com>
2018-10-30 19:23:10 -0600

    first commit

--- tarea 1 estructura/main.cpp
+++ tarea 1 estructura/main.cpp
@@ -7,9 +7,70 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main() {
+    int mat[4][4];
+    int x, n, r;
+    cin >> n;
+    for(int i=0; i<4; i++){
+        for(int c=0; c<4; c++){
+            cin >> x;
+            mat[i][c]=x;
+        }
+    }
+    r = n%4;
+    if(r<0){
+        r+=4;
+    }
+    switch (r){
+        case 0:
+            for(int i=0; i<4; i++){
+                for(int c=0; c<4; c++){
+                    if (c==3){
+                        cout << mat[i][c];
+                    }
+                    else
+                        cout << mat[i][c] << " ";
+                }
+                cout << endl;
+            }
+            break;
+        case 1:
+            for(int i=0; i<4; i++){
+                for(int c=3; c>=0; c--){
+                    if (c==0){
+                        cout << mat[c][i];
+                    }
+                    else
+                        cout << mat[c][i] << " ";
+                }
+                cout << endl;
+            }
+            break;
+        case 2:
+            for(int i=3; i>=0; i--){
+                for(int c=3; c>=0; c--){
+                    if (c==0){
+                        cout << mat[i][c];
+                    }
+                    else
+                        cout << mat[i][c] << " ";
+                }
+                cout << endl;
+            }
+            break;
+        case 3:
+            for(int i=3; i>=0; i--){
+                for(int c=0; c<4; c++){
+                    if (c==3){
+                        cout << mat[c][i];
+                    }
+                    else
+                        cout << mat[c][i] << " ";
+                }
+                cout << endl;
+            }
+            break;
+    }
     return 0;
 }
